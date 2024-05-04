42c6bdc11ca5bf8853a9f2a3d6f843e710398833
Joseph <zhaoxing@andrew.cmu.edu>
2024-05-03 23:06:00 -0700

    build new

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,9 +1,9 @@
 const dev = {
-    API_URL: 'http://a1f6946efd5304f7db5f33a15e10bbef-1209942196.us-east-2.elb.amazonaws.com:80',
+    API_URL: 'http://127.0.0.1:5000',
 };
 
 const prod = {
-    API_URL: '',
+    API_URL: 'http://a1f6946efd5304f7db5f33a15e10bbef-1209942196.us-east-2.elb.amazonaws.com',
 };
 
 const config = process.env.NODE_ENV === 'development' ? dev : prod;
